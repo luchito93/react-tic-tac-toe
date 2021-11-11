25214b24b337ed3695e6b40ed551a2e3822cc3af
Luis <luisgomez@unisangil.edu.co>
2021-11-11 18:43:19 -0500

    feat: :sparkles: controlled components
    the squere is now controlled by the board component

--- src/Board.js
+++ src/Board.js
@@ -2,8 +2,26 @@ import { Component } from "react";
 import Square from "./Square";
 
 class Board extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            squeres: Array(9).fill(null)
+        }
+    }
+
+    handleClick (squerNumber) {
+        console.log("hize click" + squerNumber)
+        const copySqueres = this.state.squeres.slice()
+        copySqueres[squerNumber] = 'X'
+        this.setState({squeres: copySqueres})
+    }
+
+
     renderSquare(i) {
-        return <Square key={i} value={i}/>
+        return (<Square
+            key={i}
+            value={this.state.squeres[i]}
+            onClickAccion={() => this.handleClick(i) }/>)
     }
     render() {
         const status = "Next player: X"
--- src/Square.js
+++ src/Square.js
@@ -1,18 +1,13 @@
 import { Component } from "react";
 
 class Square extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value: null
-        }
-    }
 
     render() {
+        const { value, onClickAccion } = this.props
         return <button
             className="square"
-            onClick={() => this.setState({value: 'X'})}>
-                {this.state.value}
+            onClick={() => onClickAccion()}>
+                {value}
         </button>
     }
 }
